c45b4cdc13ff5527a080e20ad14a5ca18abc7e46
bappydas5689@gmail.com <bappydas5689@gmail.com>
2026-06-19 10:32:05 -0700

    fixedgit status yay

--- src/com/MyLoginWeb/SaltManager.java
+++ src/com/MyLoginWeb/SaltManager.java
@@ -3,15 +3,24 @@ package com.MyLoginWeb;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Objects;
+
 public class SaltManager implements PasswordEncoder {
     @Override
     public @Nullable String encode(@Nullable CharSequence rawPassword) {
-    return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(rawPassword);
+        sb.append(rawPassword.hashCode());
+        sb.append(rawPassword.toString().length());
+        sb.append(rawPassword.charAt(0));
+        sb.append(rawPassword.charAt(1));
+        sb.append(rawPassword.charAt(rawPassword.length() - 1));
+        return sb.toString();
     }
 
     @Override
     public boolean matches(@Nullable CharSequence rawPassword, @Nullable String encodedPassword) {
-        return false;
+        return Objects.equals(this.encode(rawPassword), encodedPassword);
     }
 
 }
